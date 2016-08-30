b7d14dbe948183ad38bfb30df4dfe038fe6c01fa
miyajan <miyajan777@gmail.com>
2016-08-30 23:02:21 +0900

    add test cases

--- test/test.js
+++ test/test.js
@@ -6,4 +6,12 @@ describe('fizzbuzz', function() {
     it('returns fizzbuzz when divided by 15', function() {
         assert(fizzbuzz(45) === 'fizzbuzz');
     });
+
+    it('returns fizz when divided by 3', function() {
+        assert(fizzbuzz(9) === 'fizz');
+    });
+
+    it('returns buzz when divided by 5', function() {
+        assert(fizzbuzz(10) === 'buzz');
+    });
 });
